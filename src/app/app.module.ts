b6ece796cc3119c5cea6ee694bcf54200ce66737
Matheus Silveira <matheus.silveiraw@gmail.com>
2025-03-17 11:10:53 -0300

    componente da lista de tarefas em criação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemListComponent } from './components/item-list/item-list.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemListComponent
+    ItemListComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/task-list/task-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent {
+  tasks = [
+    { id: 1, title: 'Estudar Angular', completed: false },
+    { id: 2, title: 'Fazer compras', completed: false }
+  ];
+}
