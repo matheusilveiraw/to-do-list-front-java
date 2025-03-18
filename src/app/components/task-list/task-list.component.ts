7c96c40b22421774eec39942f0884c59ef0fbec4
Matheus Silveira <matheus.silveiraw@gmail.com>
2025-03-18 15:17:48 -0300

    titulo correto para cada lista

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
@@ -8,6 +8,7 @@ import { TodoService } from 'src/app/services/todo.service';
 })
 export class TaskListComponent implements OnInit {
   tasks: any[] = [];
+  @Input() listTitle: string = '';
 
   constructor(private todoService: TodoService) {}
 
